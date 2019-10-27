c5d66612de0dff9e1bc42ca767982bf17b1bf4ad
Damien Maguire <listerlt1@gmail.com>
2019-10-27 12:28:10 +0000

    Add files via upload

---
+++ Software/gs450h_v2.ino
@@ -0,0 +1,242 @@
+/*Basic software to run the Lexus GS450H hybrid transmission and inverter using the open source V1 controller
+ * Take an analog throttle signal and converts to a torque command to MG1 and MG2
+ * Feedback provided over USB serial
+ * 
+ * Copyright 2019 T.Darby , D.Maguire
+ * openinverter.org
+ * evbmw.com
+ * 
+ */
+
+
+#include <Metro.h>
+
+#define MG2MAXSPEED 10000
+
+
+#define PARK 0
+#define REVERSE 1
+#define NEUTRAL 2
+#define DRIVE 3
+
+Metro timer_htm=Metro(10); 
+
+byte mth_data[100];
+byte htm_data_setup[80]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,25,0,0,0,0,0,0,0,128,0,0,0,128,0,0,0,37,1};
+byte htm_data[80]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,255,0,0,0,0,0,0,0,0,0}; 
+
+unsigned short htm_checksum=0, 
+               mth_checksum=0, 
+               since_last_packet=4000;
+
+unsigned long  last_packet=0;
+            
+volatile byte mth_byte=0;
+
+float dc_bus_voltage=0,temp_inv_water=0,temp_inv_inductor=0; //just used for diagnostic output
+
+short mg1_torque=0,
+      mg2_torque=0,
+      mg1_speed=-1,
+      mg2_speed=-1;
+       
+byte inv_status=1,
+     gear=DRIVE; //get this from your transmission
+     
+bool htm_sent=0, 
+     mth_good=0;
+
+
+int Throt1Pin = A0; //throttle pedal analog inputs
+int Throt2Pin = A1;
+int ThrotVal=0; //value read from throttle pedal analog input
+     
+
+short get_torque()
+{
+  //accelerator pedal mapping to torque values here
+  ThrotVal=analogRead(Throt1Pin); //75 to 370
+  if (ThrotVal<80) ThrotVal=75;//dead zone at start of throttle travel
+  ThrotVal = map(ThrotVal, 75, 370, 0, 1000);
+  return ThrotVal; //return torque
+}
+
+#define SerialDEBUG SerialUSB
+
+void setup() {
+  
+  #define pin_inv_req 22
+  pinMode(pin_inv_req, OUTPUT); digitalWrite(pin_inv_req, 1);
+  pinMode(13, OUTPUT);  //led
+
+  Serial1.begin(250000);
+
+  PIOA->PIO_ABSR |= 1<<17;
+  PIOA->PIO_PDR |= 1<<17;
+  USART0->US_MR |= 1<<4 | 1<<8 | 1<<18;
+
+  htm_data[63]=(-5000)&0xFF;  // regen ability of battery
+  htm_data[64]=((-5000)>>8);
+
+  htm_data[65]=(27500)&0xFF;  // discharge ability of battery
+  htm_data[66]=((27500)>>8);
+ 
+  SerialDEBUG.begin(115200);
+  SerialDEBUG.print("hello world!");
+}
+
+
+void control_inverter() {
+
+  int speedSum=0;
+
+  if(timer_htm.check()) //prepare htm data
+  {
+    if(mth_good)
+    {
+      dc_bus_voltage=(((mth_data[82]|mth_data[83]<<8)-5)/2);
+      temp_inv_water=(mth_data[42]|mth_data[43]<<8);
+      temp_inv_inductor=(mth_data[86]|mth_data[87]<<8);
+      mg1_speed=mth_data[6]|mth_data[7]<<8;
+      mg2_speed=mth_data[31]|mth_data[32]<<8;
+    }
+
+    mg2_torque=get_torque(); // -3500 (reverse) to 3500 (forward)
+    mg1_torque=((mg2_torque*5)/4);
+    if((mg2_speed>MG2MAXSPEED)||(mg2_speed<-MG2MAXSPEED))mg2_torque=0;
+    if(gear==REVERSE)mg1_torque=0;
+
+    //speed feedback
+    speedSum=mg2_speed+mg1_speed;
+    speedSum/=113;
+    htm_data[0]=(byte)speedSum;
+    htm_data[75]=(mg1_torque*4)&0xFF;
+    htm_data[76]=((mg1_torque*4)>>8);
+    
+    //mg1
+    htm_data[5]=(mg1_torque*-1)&0xFF;  //negative is forward
+    htm_data[6]=((mg1_torque*-1)>>8);
+    htm_data[11]=htm_data[5];
+    htm_data[12]=htm_data[6];
+
+    //mg2
+    htm_data[26]=(mg2_torque)&0xFF; //positive is forward
+    htm_data[27]=((mg2_torque)>>8);
+    htm_data[32]=htm_data[26];
+    htm_data[33]=htm_data[27];
+
+    //checksum
+    htm_checksum=0;
+    for(byte i=0;i<78;i++)htm_checksum+=htm_data[i];
+    htm_data[78]=htm_checksum&0xFF;
+    htm_data[79]=htm_checksum>>8;
+  }
+  
+  since_last_packet=micros()-last_packet;
+
+  if(since_last_packet>=4000) //read mth
+  {    
+    htm_sent=0;
+    mth_byte=0;
+    mth_checksum=0;
+    
+    for(int i=0;i<100;i++)mth_data[i]=0;
+    while(Serial1.available()){mth_data[mth_byte]=Serial1.read();mth_byte++;}
+    
+    for(int i=0;i<98;i++)mth_checksum+=mth_data[i];
+    if(mth_checksum==(mth_data[98]|(mth_data[99]<<8)))mth_good=1;else mth_good=0;
+    last_packet=micros();
+    digitalWrite(pin_inv_req,0);
+  }
+
+  since_last_packet=micros()-last_packet;
+  
+  if(since_last_packet>=10)digitalWrite(pin_inv_req,1);
+
+  if(since_last_packet>=1000)
+  {
+    if(!htm_sent&&inv_status==0){for(int i=0;i<80;i++)Serial1.write(htm_data[i]);}
+    else if(!htm_sent&&inv_status!=0){for(int i=0;i<80;i++)Serial1.write(htm_data_setup[i]);if(mth_data[1]!=0) inv_status--;}
+    htm_sent=1;
+  }
+}
+
+
+
+
+void diag_mth()
+{
+  ///mask just hides any MTH data byte which is represented here with a 0. Useful for debug/discovering.
+  bool mth_mask[100] = {
+    0,0,0,0,0,0,0,0,1,1,
+    1,1,0,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,
+    1,0,0,1,1,1,1,0,0,1,
+    1,1,0,0,1,1,1,1,1,1,
+    1,1,1,1,1,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    1,1,0,0,1,1,0,0,1,1,
+    1,1,1,1,1,1,1,1,0,0,};
+    
+  SerialDEBUG.print("\n");
+  SerialDEBUG.println("\t0\t1\t2\t3\t4\t5\t6\t7\t8\t9");
+  SerialDEBUG.println("   ------------------------------------------------------------------------------");  
+  for (int j=0;j<10;j++)
+  {
+    SerialDEBUG.print(j*10);if(j==0)SerialDEBUG.print("0");SerialDEBUG.print(" |\t");
+    for (int k=0;k<10;k++)
+    {
+      if(mth_mask[j*10+k])SerialDEBUG.print(mth_data[j*10+k]);else SerialDEBUG.print (" ");
+      SerialDEBUG.print("\t");
+    }
+    SerialDEBUG.print("\n");
+  }
+  SerialDEBUG.print("\n");
+    
+  SerialDEBUG.print("MTH Valid: ");if(mth_good)SerialDEBUG.print("Yes"); else SerialDEBUG.print("No");SerialDEBUG.print("\tChecksum: ");SerialDEBUG.print(mth_checksum);
+  SerialDEBUG.print("\n");
+  
+  SerialDEBUG.print("DC Bus: ");if(dc_bus_voltage>=0)SerialDEBUG.print(dc_bus_voltage);else SerialDEBUG.print("----");
+  SerialDEBUG.print("v\n");
+ 
+  SerialDEBUG.print("MG1 - Speed: ");SerialDEBUG.print(mg1_speed);
+  SerialDEBUG.print("rpm\tPosition: ");SerialDEBUG.print(mth_data[12]|mth_data[13]<<8);
+  SerialDEBUG.print("\n");
+  
+  SerialDEBUG.print("MG2 - Speed: ");SerialDEBUG.print(mg2_speed);
+  SerialDEBUG.print("rpm\tPosition: ");SerialDEBUG.print(mth_data[37]|mth_data[38]<<8);
+  SerialDEBUG.print("\n");
+  
+  SerialDEBUG.print("Water Temp:\t");SerialDEBUG.print(temp_inv_water);
+  SerialDEBUG.print("c\nInductor Temp:\t" );SerialDEBUG.print(temp_inv_inductor);
+  SerialDEBUG.print("c\nAnother Temp:\t");SerialDEBUG.print(mth_data[88]|mth_data[89]<<8);
+  SerialDEBUG.print("c\nAnother Temp:\t");SerialDEBUG.print(mth_data[41]|mth_data[40]<<8);
+  SerialDEBUG.print("c\n");
+  
+  SerialDEBUG.print("\n");
+  SerialDEBUG.print("\n");
+  SerialDEBUG.print("\n");
+  SerialDEBUG.print("\n");
+  SerialDEBUG.print("\n");
+  SerialDEBUG.print("\n");
+  SerialDEBUG.print("\n");
+  SerialDEBUG.print("\n");
+  SerialDEBUG.print("\n");
+  SerialDEBUG.print("\n");
+}
+
+
+
+
+Metro timer_diag = Metro(700);
+
+void loop() {
+  control_inverter();
+  if(timer_diag.check())
+  {
+  diag_mth();
+ //SerialDEBUG.println(ThrotVal);
+  digitalWrite(13,!digitalRead(13));
+  }
+}
